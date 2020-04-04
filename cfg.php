d34be6f0fc56c17e45d3153f84d2423537337d67
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-04-04 11:30:29 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,20 @@
+<?php
+/*configs by HACK-TO-WIN*/
+
+//video data
+$video_authentication = "MzEzYWE2NjA1MDhiYzM2ZTVkZWEwNWE0MzE3YzRjNmI=";
+$token = "7d872e8b87a04fae87a64517ef405ed9";
+$deviceId = "309c23b6fa3570413b6fa357041309c2";
+$video_key = "VD2XV+wfbqgCE4xZhGy9YM9zPw18EkrzOsQ9iGjw7mZ+VKO16UYouRF++Ffe7SijVrW+x40tfwdYcHGcbHML35yDz0NBZfgFYp7iG5X101G6e+wErpwhRZUaKq7Ct8h2s8FtN6/mbOEsQhEIXgDbq54VV5dwlgg7Kx/tDfF6AHo=";
+$gaid = "95591a9b-2ca2-4b49-85fb-241b409df621";
+$video_data = "accessKeyId=MoeCube-7cc405348a6b11e89bddc85b762def01&versionCode=1.1.1&nonce=48b704eb-756d-4c55-8958-4677cbd55a16&version=1&data=rEIJyBOZhkKb7jFvxy6KQjvwf6NhnTqA4xy7H%2BPkPM8%3D";
+
+//flappybird  data
+$flyb_authentication = "NDk5NmE2MjViY2IwZTE2NWNjODQzNDI3MzZhZTZmMTM=";
+$flyb_key = "ECYdisqqnbwKIUupL/iO9o1E0W8s7Gfl5XlnkUurmpFyuYukROwKud3TyvmLKOMK9xiAFLWr7WXC4TeJiC88+wn3/KgX+/ZzwoW57VLroolFNTqYGpjq5ze4AE5WaTNQmOmd/HcgrLI7ubsoptpi4F6HJobKA86VI4jbc/dCk0s=";
+$flyb_data = "accessKeyId=MoeCube-7cc405348a6b11e89bddc85b762def01&versionCode=1.1.1&nonce=ecde19b2-8d17-425d-ac4f-a44ecfb64c0a&version=1&data=f5N2sOJoNtiJ4mTerIKCy2Em6h%2FBf0BhOu3l3zgls4g%3D";
+
+//spin data
+$spin_authentication = "ZjMxMzdlOTI5NGNjZjIwNTI3NmU3YjFjYzVmZTkwN2E=";
+$spin_key = "M50y5Jtx4C1tDjhkb3KdDv03wh4kaKDVQhnaVzkr8Yb+Z4/Icxu9k+fR05MpRfM3AxdRMtdHrIOK1b2uuBt1/WGhqe2AhzHeb0IWaVfjEhOvhsId5HvLHp/SFspLDVUXeJ1R4ebpuII4uYHEN6eRTafwfx6yB6G3fOrO4XZILYc=";
+$spin_data = "accessKeyId=MoeCube-7cc405348a6b11e89bddc85b762def01&versionCode=1.1.1&nonce=76bb19d2-0409-448b-95bf-5cb0578995b3&version=1&data=WMXA6%2B%2Ffu%2FVZ1zSI1DTVpHx3l%2F%2FjfJM%2B33Bqw9tm2MjE%2B7K8iEqA85tw7c2zFZS%2F";
